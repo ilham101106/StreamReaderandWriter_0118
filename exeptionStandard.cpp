8fe172749de83cab1b1b6a8107b57f5776f5c883
ilham101106 <pesatoffical321@gmail.com>
2025-06-10 18:39:26 +0700

    membuat pesan array intger 3 elemen dan memanggil array elemen ke 5

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -7,3 +7,13 @@ using namespace std;
 
 int main()
 {
+    cout<<"Awal Program"<<endl; //penanda 1...
+    try{
+        array<int, 3> data = { 10, 20 , 30 };
+        //pesan arary integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5 
+
+    }
+
+  
