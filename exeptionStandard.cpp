8553a05a6417f13871507b4b641458626127ac92
ilham101106 <pesatoffical321@gmail.com>
2025-06-10 18:43:10 +0700

    penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -23,6 +23,6 @@ int main()
     }
     cout<<"Baris Program Yang terakhir"<<endl;
     /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
-    return 0;
+    return 0; 
 }
 
