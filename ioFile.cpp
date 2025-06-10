51e312c4ff112a7a9cd0040e1875b8491d5bdb67
ilham101106 <pesatoffical321@gmail.com>
2025-06-10 17:44:08 +0700

    melakukan perulangan setiap baris  dengan membuat  while (getline(infile, baris))

--- ioFile.cpp
+++ ioFile.cpp
@@ -30,7 +30,13 @@ int main(){
     //jika file ada maka 
     if (infile.is_open())
     {
+        //melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
 
-        
+            
+        }
+
+
     }
-}   
+}  
